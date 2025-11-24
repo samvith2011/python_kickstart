c51c90d033ce8e29a72fca0481dcf0d9556a86a9
samvith2011 <samvith.p.rao@gmail.com>
2025-11-23 18:10:33 -0800

    Changed files

--- Openhpi/assignment wk 6.py
+++ Openhpi/assignment wk 6.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 query = input("What is your search query for apple music? ")
 query2 = query.replace(" ","%20")
-response = requests.get(f"https://music.apple.com/us/search?term={query2}")
+response = f"https://music.apple.com/us/search?term={query2}"
 
 
 
