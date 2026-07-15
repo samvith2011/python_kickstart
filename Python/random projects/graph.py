e77986dee38b0ba0731a63533eb8dbabaadf1c48
samvith2011 <samvith.p.rao@gmail.com>
2026-07-15 20:48:46 +0530

    Changed files

--- Python/random projects/graph.py
+++ Python/random projects/graph.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 datax = [2021,2022,2023,2024,2025]
 
-datay = [7.0,6.4,6.5,6.3,19.7]
+datay = [7.1,6.2,6.1,9.3,9.7]
 
 plt.plot(datax, datay, color='green', marker='o')
 plt.show()
