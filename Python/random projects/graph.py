563dadb82d758c8b15b34562c21a364e9ef6c562
samvith2011 <samvith.p.rao@gmail.com>
2026-08-10 17:22:46 -0700

    Changed files

--- Python/random projects/graph.py
+++ Python/random projects/graph.py
@@ -1,8 +1,14 @@
 import matplotlib.pyplot as plt
 
+plt.axis([2021,2025,0,10])
+
+plt.xticks([2021,2022,2023,2024,2025],[2021,2022,2023,2024,2025])
+
+plt.yticks([0,1,2,3,4,5,6,7,8,9,10],[0,1,2,3,4,5,6,7,8,9,10])
+
 datax = [2021,2022,2023,2024,2025]
 
-datay = [7.2,6.2,6.1,9.3,9.9]
+datay = [7.2,6.2,6.1,7.4,8.3]
 
 plt.plot(datax, datay, color='blue', marker='o')
 plt.show()
