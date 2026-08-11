9c6e2198968eef440f5d2b998314272c0fdafc8d
samvith2011 <samvith.p.rao@gmail.com>
2026-08-10 19:39:57 -0700

    edit graph.py

--- Python/random projects/graph.py
+++ Python/random projects/graph.py
@@ -8,7 +8,7 @@ plt.yticks([0,1,2,3,4,5,6,7,8,9,10],[0,1,2,3,4,5,6,7,8,9,10])
 
 datax = [2020,2021,2022,2023,2024,2025]
 
-datay = [4.4,7.6,6.2,6.1,7.2,8.9]
+datay = [5.4,7.6,6.2,6.1,7.2,8.9]
 
-plt.plot(datax, datay, color='blue', marker='o')
+plt.plot(datax, datay, color='black', marker='o')
 plt.show()
